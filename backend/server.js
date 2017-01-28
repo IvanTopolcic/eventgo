28e3b9c96c8679114525de5f3e945ed3e2368c22
Kartik Karkala <puranikk@web007171.wireless.mcgill.ca>
2017-01-28 15:57:31 -0500

    Basic layout view for frontend set up

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.get('/',function(req,res){
+  res.sendFile('/Users/puranikk/Documents/Repos/eventgo/frontend/index.html');
+});
 
 app.listen(3000, function () {
   console.log('EventGo started on http://localhost:3000')
