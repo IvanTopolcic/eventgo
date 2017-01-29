3df9610cae6cdbf67571e2c72eaa3f875b865de7
Kartik Karkala <kartik.karkala@mail.mcgill.ca>
2017-01-28 20:25:29 -0500

    angular components

--- frontend/app.js
+++ frontend/app.js
@@ -3,20 +3,3 @@
 // Define the `eventgoApp` module
 var eventgoApp = angular.module('eventgoApp', []);
 
-// Define the `EventgoAppController` controller on the `eventgoApp` module
-eventgoApp.controller('EventgoAppController', function EventgoAppController($scope) {
-    $scope.evnts = [
-        {
-            name: "McKibbins Irish Pub",
-            location: "Blvd St. Laurent, Montreal"
-        },
-        {
-            name: "Tokyo",
-            location: "Blvd St. Laurent, Montreal"
-        },
-        {
-            name: "Brutopia",
-            location: "Rue Crescent, Montreal"
-        }
-    ]
-});
--- frontend/modules/eventFinder/src/eventFinder.js
+++ frontend/modules/eventFinder/src/eventFinder.js
@@ -1,12 +1,23 @@
-function EventFinderController() {
+// Define the `EventgoAppController` controller on the `eventgoApp` module
 
-}
-
-angular.module('eventgoApp').component('eventFinder', {
-    templateUrl: 'eventFinderViewTemplate.html',
-    controller: 'EventFinderController',
-    bindings: {
-        name: '=',
-        location: '='
-    }
+angular
+    .module('eventgoApp')
+    .component('eventFinder', {
+        templateUrl: 'eventFinderViewTemplate.html',
+        controller: function EventgoAppController() {
+            this.evnts = [
+                {
+                    name: "McKibbins Irish Pub",
+                    location: "Blvd St. Laurent, Montreal"
+                },
+                {
+                    name: "Tokyo",
+                    location: "Blvd St. Laurent, Montreal"
+                },
+                {
+                    name: "Brutopia",
+                    location: "Rue Crescent, Montreal"
+                }
+            ];
+        }
 });
