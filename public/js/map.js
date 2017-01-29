dbe61533d94a168a1de1a9a5a8c715ae44eb4446
Kartik Karkala <kartik.karkala@mail.mcgill.ca>
2017-01-29 11:13:39 -0500

    styling

--- public/js/map.js
+++ public/js/map.js
@@ -93,15 +93,6 @@ function addMarker(evnt){
 		infowindow.open(map, marker);
 	});
 
-	var marker2 = new google.maps.Marker({
-		position: new google.maps.LatLng(45.503751, -73.577641),
-		icon: icon,
-		map: map,
-		title: 'my 2nd title'
-	});
-	google.maps.event.addDomListener(document.getElementById("event-name"), "click", function(ev) {
-		map.setCenter(marker2.getPosition());
-	});
 }
 
 /**
