eb55b5fe939a12311179f236ccd8b734e97565a7
Kartik Karkala <kartik.karkala@mail.mcgill.ca>
2017-01-29 01:20:08 -0500

    event finding layout created

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,19 @@
-'use strict';
-// Define the `eventgoApp` module
-var eventgoApp = angular.module('eventgoApp', ['ngRoute'])
+var myapp = angular.module('myapp', []);
+
+myapp.controller('EventListController', ['$scope', function($scope){
+    $scope.evnts = [
+        {
+            name: "McKibbins Irish Pub",
+            location: "Blvd St. Laurent, Montreal"
+        },
+        {
+            name: "Tokyo",
+            location: "Blvd St. Laurent, Montreal"
+        },
+        {
+            name: "Brutopia",
+            location: "Rue Crescent, Montreal"
+        }
+    ];
+}]);
 
